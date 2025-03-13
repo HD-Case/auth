8c1a177398c33cacd87bb3ce3f9d7e9141170123
Benard Haugen <ben.haugen@kellogg.northwestern.edu>
2025-03-13 15:05:17 +0000

    Lab exercise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    if session["user_id"] != nil
+      @tasks = Task.where({ "user_id" => session["user_id"]})
+    else
+      flash["notice"] = "You Need to Login"
+      redirect_to "/login"
   end
 
   def create
     @task = Task.new
     @task["description"] = params["description"]
+    @task["user_id"] = session["user_id"]
     @task.save
     redirect_to "/tasks"
   end
