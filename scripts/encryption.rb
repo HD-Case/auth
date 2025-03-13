49638846f101258bf5192e0d0b8e8db117e6ff19
Benard Haugen <ben.haugen@kellogg.northwestern.edu>
2025-03-13 12:23:48 +0000

    Code along in class

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,12 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+password = "puppies"
+encrypted_password = BCrypt::Password.create(password)
+puts encrypted_password
+
 # 2. prepare encrypted string for testing
+BCrypt::Password.new(encrypted) == secret
+
 # 3. test secret against prepared encrypted string
+BCrypt::Password.new(encrypted) == secret
